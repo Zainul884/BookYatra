68635a71395bee2fc4dbf22400ced4e7e4e3fafd
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2024-01-26 13:11:26 -0800

    header code for home page

--- bookyatraa/app/home page/home.js
+++ bookyatraa/app/home page/home.js
@@ -1,17 +1,19 @@
 export default function Home() {
   return (
-    <div className="home">
-      <header className=''>
-        <div className="">
-            <img src="../img/homepage/logo.png" alt="logo" className="w-20 h-20" />
-            <button className=''>Home</button>
-            <button className=''>Hotel</button>
-            <Link href="../qualifications" className=''>Flight</Link>
-            <Link href="../projects"className=''>Login</Link>
-            <button className=''>Sign Up</button>
-            <p className='border-b-2 border-black p-1'></p>
+        <div className="home">
+          <header className="header">
+            <div className="bookyatra">
+              <p className="text-bold text-3xl">BOOKYATRA</p>
+            </div>
+            <div className="left-buttons">
+              <button>Home</button>
+              <button>Hotel</button>
+              <button>Flight</button>
+              <button>Login</button>
+              <button>Sign Up</button>
+            </div>
+          </header>
         </div>
-        </header>
-    </div>
+
   );
 }
