bf91e7ff30fd1cc7e4cec0969f65537f72bdcb8e
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2024-01-29 13:33:46 -0800

    home page header and pic of flight and hotel

--- Team/bookyatraa/app/homepage/page.js
+++ Team/bookyatraa/app/homepage/page.js
@@ -5,25 +5,29 @@ import Link from 'next/link';
 function Home() {
   return (
 
-    <div className="home">
+    <div className="homepage">
       <header className="header">
         <div className="header-content">
-         
-          <img src="./Images Capstone/LOGO without bg.png" alt="logo" className="logo-image" />
-          <nav className="navigation">
-        
+          <img src="./Images Capstone/LOGO without bg.png" alt="logo" className="logo-home" />
+          <nav className="homenavigation">
             <Link href="/home"className="link">Home</Link>
             <Link href="/hotel" className="link">Hotel</Link>
             <Link href="/flight"className="link">Flight</Link>
             <Link href="/login" className="link">Login</Link>
             <Link href="/signup" className="link">Sign Up</Link>
           </nav>
-          <p className="underline"></p>
         </div>
+        <hr className="line"></hr>
       </header>
       <main>
-       
-        <h1>Welcome to the Home Page</h1>
+        <div className="flight-hotel">
+          <div className="flight">
+            <img src="./Images Capstone/homePageflight.jpg.jpg" alt='flight' className='flight-pic'/>
+          </div>
+          <div className="hotel">
+            <img src="./Images Capstone/homePagehotel.jpg" alt='hotel' className='hotel-pic'/>
+          </div>
+        </div>
       </main>
     </div>
   );
